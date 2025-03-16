db25dfac64d54450e6d20dd30a796dca9a9a076b
Quang Thinh Le <quangthinh2924@gmail.com>
2025-03-16 21:18:27 +0700

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/find-smallest-number-game/sw.js', { scope: '/find-smallest-number-game/' })})}
