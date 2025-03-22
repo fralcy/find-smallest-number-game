83a3393755240fd0ae5f7defc17300e68134ac61
Quang Thinh Le <quangthinh2924@gmail.com>
2025-03-22 14:14:38 +0700

    update ui

--- find-smallest-number-game/dev-dist/sw.js
+++ find-smallest-number-game/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-1cfa77fd'], (function (workbox) { 'use strict';
     "revision": "a8b973d2b39152e241a80b0aba40e299"
   }, {
     "url": "index.html",
-    "revision": "0.4mtr6no6ilo"
+    "revision": "0.ek5thviqtf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
